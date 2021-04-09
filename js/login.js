f6b210de6278f9d49cd8830c838aec32174aff86
Jesica Viola <jesica.viola@gmail.com>
2021-04-08 23:09:10 -0300

    login inputs required validation

--- js/login.js
+++ js/login.js
@@ -10,3 +10,10 @@ var inputs = document.getElementsByTagName("input");
 if (inputs.length !== 2) {
   errors.push ('<li>the document should have 2 inputs</li>')
 }
+
+for (var i = 0; i < inputs.length; i++) {
+  if(inputs[i].required === false) {
+    errors.push ('<li>inputs must be required</li>')
+  }
+}
+
